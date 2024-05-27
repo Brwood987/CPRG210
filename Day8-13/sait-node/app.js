3a3bda9bceabcc4edf59b6314680a0c99c27631d
Braindamaged69 <technowizard30@gmail.com>
2024-05-27 10:13:50 -0600

    More notes

--- Day8-13/sait-node/app.js
+++ Day8-13/sait-node/app.js
@@ -5,16 +5,29 @@ const http = require("http");
 //Load FS module
 const fs = require("fs");
 
+const url = require("url");
+
+
+
 //Create the http server
 http.createServer((request, response) =>{
 
-    fs.readFile("demo.html", (err, data) => {
+    let parsedAddress = url.parse(request.url, true);
+    let file = "." + parsedAddress.pathname;
+
+    fs.readFile(file, (err, data) => {
         //set the response HTTP header with http status and content
         response.writeHead(200, { "Content-Type": "text/html"});
 
         //send the response body "Hello World"
         response.write(data);
         response.end();
+
+        if (err) {
+            response.writeHead(404, {"Content-Type": "text/html"});
+            response.write("<h1>404: Page Not Found<h1>");
+            return response.end();
+        }
     })
     
     //pass a function as the 2nd argument to listen() so that we log a
