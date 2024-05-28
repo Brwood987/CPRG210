4f6851552b8311383eb7d0d8df120cf455cd244d
Ben Wood <benjamin.wood@edu.sait.ca>
2024-05-27 22:13:34 -0600

    Added references to all the pages

--- Day8-13/Assignment/Index.js
+++ Day8-13/Assignment/Index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 8000;
 
 app.listen(port, () => {
-    console.log("Server is listening on port ${port}. Ready to accept requests.");
+    console.log("Server is listening on port " + port + ". Ready to accept requests.");
 });
 
 //default page
@@ -11,39 +12,29 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-// login page? Have to type /login in the browser??
-app.get("/login", (req, res) => {
-    res.send("<form method = 'POST'><input type = 'text' name 'username' ")
-})
+//about page
+app.get("/about", (req, res) => {
+    res.sendFile(`${__dirname}/pages/about.html`);
+});
 
-//once user logs in, send a post message that approves the login
-app.post("/login", (req, res) => {
-    res.send("<h1>Login Processed</h1>");
+//contact page
+app.get("/contact", (req, res) => {
+    res.sendFile(`${__dirname}/pages/contact.html`);
+});
+
+//home page
+app.get("/home", (req, res) => {
+    res.sendFile(`${__dirname}/pages/home.html`);
 });
 
 //pick from 4 greeting files
 app.get("/greet", (req, res) => {
     var file = greetings[Math.floor(Math.random() * greetings.length)] + ".html";
-    res.sendFile('$(_dirname)/greetings/$(file)');
+    res.sendFile(`${__dirname}/greetings/${file}`);
 });
-
-//check to see if adding .html to the path solves things. 
-//Makes it so you can just type the file name instead of the name +.html
-app.use(express.static("views", {
-    extensions: ["html"]
-}));
-
-//allows express to access the public folder
-app.use(express.static("public"));
-
 
 app.use(checkNotFound);
 
 function checkNotFound(req, res, next){
     res.status(404).send("File Not Found");
 }
-
-
-//app.use((req, res, next) => {
-  //  res.status(404).send("Sorry I can't find that!");
-//});
