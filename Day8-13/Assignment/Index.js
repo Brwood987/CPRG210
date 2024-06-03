25ff71f7d026548d67bbe85ac8be0167ecd6460c
Ben Wood <benjamin.wood@edu.sait.ca>
2024-06-02 22:20:44 -0600

    Added pug templates

--- Day8-13/Assignment/Index.js
+++ Day8-13/Assignment/Index.js
@@ -3,38 +3,42 @@ const app = express();
 const path = require("path");
 const port = 8000;
 
+// Set the view engine to Pug
+app.set('view engine', 'pug');
+app.set('pages', path.join(__dirname, 'pages'));
+
 app.listen(port, () => {
     console.log("Server is listening on port " + port + ". Ready to accept requests.");
 });
 
-//default page
+// Default page
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.render("index", { title: "Hello World!" });
 });
 
-//about page
+// About page
 app.get("/about", (req, res) => {
-    res.sendFile(`${__dirname}/pages/about.html`);
+    res.render("about", { title: "About Us" });
 });
 
-//contact page
+// Contact page
 app.get("/contact", (req, res) => {
-    res.sendFile(`${__dirname}/pages/contact.html`);
+    res.render("contact", { title: "Contact Us" });
 });
 
-//home page
+// Home page
 app.get("/home", (req, res) => {
-    res.sendFile(`${__dirname}/pages/home.html`);
+    res.render("home", { title: "Home" });
 });
 
-//pick from 4 greeting files
+// Pick from 4 greeting files
 app.get("/greet", (req, res) => {
-    var file = greetings[Math.floor(Math.random() * greetings.length)] + ".html";
-    res.sendFile(`${__dirname}/greetings/${file}`);
+    var file = greetings[Math.floor(Math.random() * greetings.length)];
+    res.render(`greetings/${file}`, { title: "Greeting" });
 });
 
 app.use(checkNotFound);
 
-function checkNotFound(req, res, next){
+function checkNotFound(req, res, next) {
     res.status(404).send("File Not Found");
 }
